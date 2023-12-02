ae3d7a86be1bf42f137b908818b329f61f565783
Peter Simone <plssimone@gmail.com>
2023-12-02 13:46:42 -0500

    hotfix: update site copy

--- components/sections/Home.tsx
+++ components/sections/Home.tsx
@@ -10,10 +10,10 @@ export default (): JSX.Element => {
       <div className={styles.section_div}>
         <div className={styles.home_text}>
           <h1 className="text-grad-light">
-            Ensuring <br /> quality audits
+            Facilitating <br /> quality audits
           </h1>
           <p className="text-grad-light">
-            On-chain solution for establishing terms and carrying out smart contract audits
+            On-chain solution for facilitating high fidelity smart contract audits
           </p>
           <a href="https://docs.bevor.io" referrerPolicy="no-referrer" target="_blank">
             <Button theme="light">
